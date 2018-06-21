d97dbfe0bf29d0d77c4518302263c78fc92d7c77
Batidogo <33965170+Batidogo@users.noreply.github.com>
2018-06-20 20:14:37 -0500

    lista de interesados
    Se agregó el método para obtener la lista de interesados (Todos los registros de la tabla Transacciones relacionados con un producto en base a fkproducto).

--- src/java/Controladores/ControladorTransaccion.java
+++ src/java/Controladores/ControladorTransaccion.java
@@ -11,7 +11,7 @@ import java.util.List;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
-
+import java.util.ArrayList;
 /**
  *
  * @author Leonardo Martinez
@@ -20,6 +20,7 @@ import javax.inject.Named;
 @SessionScoped
 public class ControladorTransaccion implements Serializable{
     private Transaccion transaccion;
+    private  private int productoId;
     @EJB
     private FachadaTransaccion fachada;
     
@@ -44,5 +45,23 @@ public class ControladorTransaccion implements Serializable{
     
     public List<Transaccion> getTransaccions() {
         return getFachada().findAll();
+    }
+
+     public void setProductoId(int productoId){
+    this.productoId=productoId;
+    }
+    
+    public List<Transaccion> ListaInteresados(){
+       
+    List<Transaccion> listaDeInteresados = new ArrayList<>();
+    int producto;
+    String correo;
+    List<Transaccion> transacciones = getFachada().findAll();    
+    for(int i=0;i<transacciones.size();i++){
+    producto=transacciones.get(i).getFkproducto().getPkproducto();
+        if(producto==this.productoId)
+        listaDeInteresados.add(transacciones.get(i));
+    }
+    return listaDeInteresados;
     }
 }
